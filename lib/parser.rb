c8bb1bf6a6cd5ae069e5546a947050e562d08e83
ruby_road_to_ruin <zac.palmquist@gmail.com>
2017-12-22 02:02:59 -0700

    style check. passing

--- lib/parser.rb
+++ lib/parser.rb
@@ -7,7 +7,6 @@ attr_reader :request_line,
             :ip,
             :port,
             :value,
-            :cache,
             :accept
 
   def initialize(request_line)
@@ -15,7 +14,6 @@ attr_reader :request_line,
     @verb, @path, @protocol = request_line[0].split(" ")
     @host, @ip, @port       = request_line[1].split(":")
     @path, @value           = path.split("?word=")
-    @cache                  = request_line[3].split[1]
     @accept                 = request_line[6]
   end
 end
