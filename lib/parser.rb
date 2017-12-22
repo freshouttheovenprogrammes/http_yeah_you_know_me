3f20eed60e5247979dd19772e5a17741efb8ce2c
ruby_road_to_ruin <zac.palmquist@gmail.com>
2017-12-22 02:49:31 -0700

    so close to solving this word_search issue

--- lib/parser.rb
+++ lib/parser.rb
@@ -7,7 +7,8 @@ attr_reader :request_line,
             :ip,
             :port,
             :value,
-            :accept
+            :accept,
+            :word_search_param
 
   def initialize(request_line)
     @request_line           = request_line
@@ -15,5 +16,6 @@ attr_reader :request_line,
     @host, @ip, @port       = request_line[1].split(":")
     @path, @value           = path.split("?word=")
     @accept                 = request_line[6]
+    @word_search_param      = request_line[0].split("?")[1].split("=")[0] unless request_line[0].split[1].split("?")[1].nil?
   end
 end
