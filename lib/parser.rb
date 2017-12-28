e2822088d5af408941efcebf0916fc8def39d349
ruby_road_to_ruin <zac.palmquist@gmail.com>
2017-12-27 17:21:41 -0700

    add notes with some findings for next iteration of refactoring

--- lib/parser.rb
+++ lib/parser.rb
@@ -19,3 +19,22 @@ attr_reader :request_line,
     @word_search_param      = request_line[0].split("?")[1].split("=")[0] unless request_line[0].split[1].split("?")[1].nil?
   end
 end
+
+
+=begin
+12] pry(#<Parser>)> path.split("word_search?")
+=> ["/", "word=chun"]
+[13] pry(#<Parser>)> path.split("word_search?").split("=")
+NoMethodError: undefined method `split' for ["/", "word=chun"]:Array
+from (pry):18:in `initialize'
+[14] pry(#<Parser>)> path.split("word_search?")[1].split("=")
+=> ["word", "chun"]
+[15] pry(#<Parser>)> path.split("word_search?")[1].split("=")[1]
+=> "chun"
+[16] pry(#<Parser>)> path.split("word_search?")[1].split("=")
+=> ["word", "chun"]
+[17] pry(#<Parser>)> anything, value = path.split("word_search?")[1].split("=")
+=> ["word", "chun"]
+[18] pry(#<Parser>)> value
+=> "chun"
+=end
